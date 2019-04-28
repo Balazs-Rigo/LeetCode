f8d8b9e073b7e48304078d3144bdf1d5bf45a989
Balázs Rigó <rigo.balazs86@gmail.com>
2019-04-28 22:58:50 +0200

    Leet Code 28as feladata elkezdve

---
+++ LeetCode/LeetCode/Easy/number28ImplementstrStr.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Easy
+{
+    class number28ImplementstrStr
+    {
+        public static int StrStr(string haystack, string needle)
+        {
+            bool isStillEqual = true;
+            for (int i = 0; i < haystack.Length; i++)
+            {
+                for (int j = 0; j < needle.Length; j++)
+                {
+                    if (haystack[i] == needle[i])
+                    {
+
+                    }                    
+                }
+                
+            }
+
+            return 0;
+        }
+    }
+}
